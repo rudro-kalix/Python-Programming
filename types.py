90a8ba3f3cbd96ebbc6a7560268eac6edb3b3f7e
Julkar Nayen Bin Hossain <pubgfukedup@gmail.com>
2026-05-05 13:05:17 +0600

    2026-05-05_13:05

--- types.py
+++ types.py
@@ -4,9 +4,9 @@ z= "oh i see"
 i= "@"
 j= False 
 k= None
-print ("the type of x is" ,type(x))
-print ("the type of y is" ,type(y))
-print ("the type of z is" ,type(z))
-print ("the type of i is" ,type(i))
-print ("the type of j is" ,type(j))
-print ("the type of k is" ,type(k))
+print ("the type of x = \"",x,"\" is" ,type(x))
+print ("the type of y = \"",y,"\" is" ,type(y))
+print ("the type of z = \"",z,"\" is" ,type(z))
+print ("the type of i = \"",i,"\" is" ,type(i))
+print ("the type of j = \"",j,"\" is" ,type(j))
+print ("the type of k = \"",k,"\" is" ,type(k))
