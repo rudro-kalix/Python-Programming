dececf7b28ff859d05fa9ab937d56465a050fb84
Julkar Nayen Bin Hossain <pubgfukedup@gmail.com>
2026-05-05 13:08:33 +0600

    2026-05-05_13:08

---
+++ python_basic_calculator.py
@@ -0,0 +1,7 @@
+a= 10
+b=20
+
+print("the sum of ",a," and ",b," is :" ,a+b)
+print("the sub of ",a," and ",b," is :", a-b)
+print("the div of ",a," and ",b," is :" ,a/b)
+print("the mul of ",a," and ",b," is :", a*b)
